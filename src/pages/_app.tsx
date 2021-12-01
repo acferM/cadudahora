26c97b40057bff335d6917ec8e40af70a5d0fd44
acferM <matheus.acfer@nave.org.br>
2021-12-01 00:41:11 -0300

    Project setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { AppProps } from 'next/app';
+import Global from '../styles/Global';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Global />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
