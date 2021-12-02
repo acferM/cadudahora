6fb59bdaed1df169eae439c0a765f35536de9880
acferM <matheus.acfer@nave.org.br>
2021-12-02 09:53:36 -0300

    home page done [no responsive]

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,60 @@
+import { Carousel } from 'react-responsive-carousel';
+
 import { Header } from '../components/Header';
-import { Container } from '../styles/pages/Home';
+
+import { Container, HomeSection, SliderIndicator } from '../styles/pages/Home';
+
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export default function Home() {
   return (
     <Container>
       <Header />
+
+      <HomeSection id="start">
+        <main>
+          <h1>Juntos vamos realizar o seu projeto mais DaHora!</h1>
+
+          <footer>
+            <button type="button">ORÇAMENTO</button>
+
+            <button type="button">VER PROJETOS</button>
+          </footer>
+        </main>
+
+        <aside>
+          <img src="dahora.svg" alt="DAHORA" />
+
+          <Carousel
+            axis="horizontal"
+            autoPlay
+            swipeable
+            emulateTouch
+            infiniteLoop
+            interval={3000}
+            showArrows={false}
+            showStatus={false}
+            renderIndicator={(onClickHandler, isSelected) => (
+              <SliderIndicator
+                onClick={onClickHandler}
+                isSelected={isSelected}
+              />
+            )}
+          >
+            <div className="carousel-item">
+              <p>teste 1</p>
+            </div>
+
+            <div className="carousel-item">
+              <p>teste 2</p>
+            </div>
+
+            <div className="carousel-item">
+              <p>teste 3</p>
+            </div>
+          </Carousel>
+        </aside>
+      </HomeSection>
     </Container>
   );
 }
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -1,5 +1,95 @@
 import styled from 'styled-components';
 
+interface SlideIndicatorProps {
+  isSelected: boolean;
+}
+
 export const Container = styled.div``;
 
-export const HomeSection = styled.section``;
+export const HomeSection = styled.section`
+  height: calc(100vh - 90px);
+  padding-left: 9.125rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  main {
+    h1 {
+      width: 40.875rem;
+      ${props => props.theme.colors.text};
+      ${props => props.theme.fonts.title};
+    }
+
+    footer {
+      margin-top: 1.875rem;
+      display: flex;
+      align-items: center;
+      gap: 1.25rem;
+
+      button {
+        height: 4rem;
+        width: 15.6875rem;
+        border-radius: 0.625rem;
+        ${props => props.theme.fonts.button_secondary};
+
+        &:nth-child(1) {
+          border: 0;
+          background: ${props => props.theme.colors.purple};
+          color: ${props => props.theme.colors.background_primary};
+        }
+
+        &:nth-child(2) {
+          border: 1px solid ${props => props.theme.colors.purple};
+          background: transparent;
+          color: ${props => props.theme.colors.purple};
+        }
+      }
+    }
+  }
+
+  aside {
+    display: flex;
+    height: 90%;
+    margin-bottom: auto;
+    position: relative;
+
+    img {
+      position: absolute;
+      left: -70px;
+      height: 95%;
+      top: 2%;
+    }
+
+    div.carousel-root {
+      height: 100%;
+    }
+
+    div.carousel-slider {
+      height: 100%;
+    }
+
+    div.slider-wrapper {
+      width: 58.375rem;
+      height: 100%;
+
+      ul {
+        height: 100%;
+      }
+    }
+
+    div.carousel-item {
+      height: 100%;
+      background: gray;
+    }
+  }
+`;
+
+export const SliderIndicator = styled.button<SlideIndicatorProps>`
+  aspect-ratio: 1/1;
+  height: 1.3125rem;
+  border-radius: 50%;
+  border: 0;
+  margin-left: 10px;
+  margin-right: 10px;
+  background: ${props => props.isSelected && props.theme.colors.purple};
+`;
